914b95709b0ebc7bee37b7d40f3dc512002ab481
Jonas-Desktop <studies@jonas.zengerle.email>
2024-06-01 20:06:39 +0200

    Added functionality to use the weatherstations from the database

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,12 @@
 import express from "express"
+import * as database from "../database/index.js"
 
 const router = express.Router()
 
-router.get("/", (request, response, next) => {
-        response.render("dashboard")
+router.get("/", async (request, response, next) => {
+        const get_weatherstations = await database.query("Select * from weathertop.weatherstations")
+
+        response.render("dashboard", { weatherstations: get_weatherstations.rows })
     }
 )
 
