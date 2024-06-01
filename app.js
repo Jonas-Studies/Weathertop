0ef54381bfaea5dfd16d79a37a33d95361bb0fae
Jonas-Desktop <studies@jonas.zengerle.email>
2024-06-01 19:43:34 +0200

    Added Routing Modules for each page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const PORT = process.env.PORT;
 
-import mountRoutes from "./routes/index.js";
+import mountRoutes from "./routes/routes.js";
 
 const app = express()
 
--- routes/station.js
+++ routes/station.js
@@ -3,7 +3,7 @@ import express from "express"
 const router = express.Router()
 
 router.get("/", (request, response, next) => {
-        response.render("index")
+        response.render("station")
     }
 )
 
