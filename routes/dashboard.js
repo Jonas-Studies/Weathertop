8984d92b15b79cac6238961ecdae4e1bd9077e89
Jonas-Desktop <studies@jonas.zengerle.email>
2024-06-02 15:26:57 +0200

    Changed the dashboard so that data from the latest reading is getting loaded from the database

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,7 +6,30 @@ const router = express.Router()
 router.get("/", async (request, response, next) => {
         const get_weatherstations = await database.query("Select * from weathertop.weatherstations")
 
-        response.render("dashboard", { weatherstations: get_weatherstations.rows })
+        var weatherstations = []
+
+        for (var index_of_weatherstation = 0; index_of_weatherstation < get_weatherstations.rows.length; index_of_weatherstation += 1) {
+            var weatherstation = get_weatherstations.rows[index_of_weatherstation]
+
+            const get_latestReading_by_weatherstationID = await database.query('Select * from weathertop.readings reading where reading."weatherstation_ID" = $1::uuid order by reading."created_on" desc fetch first row only', [weatherstation.ID])
+
+            if (get_latestReading_by_weatherstationID.rowCount == 1) {
+                weatherstations.push(
+                    {
+                        weatherstation: {
+                            data: weatherstation,
+                            latest_reading: get_latestReading_by_weatherstationID.rows[0]
+                        }
+                    }
+                )
+            }
+        }
+
+        console.log(weatherstations)
+        
+        console.log(weatherstations[0].weatherstation)
+
+        response.render("dashboard", { weatherstations: weatherstations })
     }
 )
 
