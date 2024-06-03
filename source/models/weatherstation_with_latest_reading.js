7e1ea3098375525b025544b23063377489f1a4e9
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-03 17:44:02 +0200

    Added the weatherstation_with_latest_reading model to the weatherstation route

--- source/models/weatherstation_with_latest_reading.js
+++ source/models/weatherstation_with_latest_reading.js
@@ -1,17 +1,23 @@
 import * as weatherstation from './weatherstation.js'
-import * as reading from 'reading.js'
+import * as reading from './reading.js'
 
 export async function get_one_by_ID (weatherstationID) {
-	const result = get_empty()
+	var result = get_empty()
 
-	const weatherstation = weatherstation_get_one_by_ID(weatherstationID)
+	const weatherstation_data = await weatherstation.get_one_by_ID(weatherstationID)
 	
-	if (weatherstation.ID != undefined) {
-		const latest_reading = reading.get_latest_by_weatherstationID(weatherstationID)
+	if (weatherstation_data.ID != undefined) {
+		const latest_reading = await reading.get_latest_by_weatherstationID(weatherstationID)
 
 		if (latest_reading.ID != undefined) {
 			result = get_new(weatherstation, latest_reading)
 		}
+		else {
+			console.info("No reading found for weatherstation " + weatherstation_data.toString())
+		}
+	}
+	else {
+		console.info("No weatherstation found for ID " + weatherstationID)
 	}
 
 	return result
--- source/routes/weatherstation.js
+++ source/routes/weatherstation.js
@@ -1,7 +1,6 @@
 import express from "express"
 
-import * as ws from "../models/weatherstation.js"
-import * as reading from "../models/reading.js"
+import * as weatherstation_with_latest_reading from "../models/weatherstation_with_latest_reading.js"
 
 const router = express.Router()
 
@@ -9,13 +8,8 @@ router.get("/", async (request, response, next) => {
 	const weatherstation_id = request.query.id
 
 	const weatherstation = {
-		weatherstation: {
-			data: await ws.get_one_by_ID(weatherstation_id),
-			latest_reading: await reading.get_latest_by_weatherstationID(weatherstation_id)
-		}
+		weatherstation: await weatherstation_with_latest_reading.get_one_by_ID(weatherstation_id)
 	}
-
-	console.log(weatherstation)
 
         response.render("station", weatherstation)
     }
