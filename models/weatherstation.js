80a16c60f0d9044de1ef3b8cd5d2b1e18b88da19
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-03 10:36:01 +0200

    Added a weatherstation model, changed the dashboard route to use it

---
+++ models/weatherstation.js
@@ -0,0 +1,34 @@
+import * as database from '../database/index.js'
+
+export const get_one_by_ID = async (ID) => {
+	var result = get_emptyWeatherstation();
+
+	const get_weatherstation = await database.query('Select * from weathertop.weatherstations weatherstation where weatherstation."ID" = $1::uuid', [ ID ]);
+
+	if (get_weatherstation.rowCount == 1) {
+		result = get_weatherstation.rows[0]
+	}
+
+	return result;
+}
+
+export async function get_many () {
+	var result = []
+
+	const get_weatherstations = await database.query('Select * from weathertop.weatherstations')
+
+	if (get_weatherstations.rowCount > 0) {
+		result = get_weatherstations.rows
+	}
+
+	return result;
+}
+
+function get_emptyWeatherstation () {
+	return {
+		ID: undefined,
+		name: undefined,
+		latitude: undefined,
+		longitude: undefined
+	}
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,35 +1,34 @@
 import express from "express"
 import * as database from "../database/index.js"
+import * as weatherstation from "../models/weatherstation.js"
+
+console.log(weatherstation)
 
 const router = express.Router()
 
 router.get("/", async (request, response, next) => {
-        const get_weatherstations = await database.query("Select * from weathertop.weatherstations")
+        const weatherstations = await weatherstation.get_many()
 
-        var weatherstations = []
+	const weatherstations_with_latestReading = []
 
-        for (var index_of_weatherstation = 0; index_of_weatherstation < get_weatherstations.rows.length; index_of_weatherstation += 1) {
-            var weatherstation = get_weatherstations.rows[index_of_weatherstation]
+        for (var index_of_weatherstation = 0; index_of_weatherstation < weatherstations.length; index_of_weatherstation += 1) {
+            var current_weatherstation = weatherstations[index_of_weatherstation]
 
-            const get_latestReading_by_weatherstationID = await database.query('Select * from weathertop.readings reading where reading."weatherstation_ID" = $1::uuid order by reading."created_on" desc fetch first row only', [weatherstation.ID])
+            const get_latestReading_by_weatherstationID = await database.query('Select * from weathertop.readings reading where reading."weatherstation_ID" = $1::uuid order by reading."created_on" desc fetch first row only', [current_weatherstation.ID])
 
             if (get_latestReading_by_weatherstationID.rowCount == 1) {
-                weatherstations.push(
+                weatherstations_with_latestReading.push(
                     {
                         weatherstation: {
-                            data: weatherstation,
+                            data: current_weatherstation,
                             latest_reading: get_latestReading_by_weatherstationID.rows[0]
                         }
                     }
                 )
             }
         }
-
-        console.log(weatherstations)
         
-        console.log(weatherstations[0].weatherstation)
-
-        response.render("dashboard", { weatherstations: weatherstations })
+        response.render("dashboard", { weatherstations: weatherstations_with_latestReading })
     }
 )
 
