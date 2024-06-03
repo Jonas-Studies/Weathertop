ff809df57240703110576ac451d7e29cf3754300
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-03 11:20:38 +0200

    Added the weatherstations url to the model

--- source/models/weatherstation.js
+++ source/models/weatherstation.js
@@ -1,34 +1,40 @@
 import * as database from '../database/index.js'
 
-export const get_one_by_ID = async (ID) => {
-	var result = get_empty()
-
-	const get_weatherstation = await database.query('Select * from weathertop.weatherstations weatherstation where weatherstation."ID" = $1::uuid', [ ID ])
-
-	if (get_weatherstation.rowCount == 1) {
-		result = get_weatherstation.rows[0]
-	}
-
-	return result
-}
-
 export async function get_many () {
 	var result = []
 
 	const get_weatherstations = await database.query('Select * from weathertop.weatherstations')
 
-	if (get_weatherstations.rowCount > 0) {
-		result = get_weatherstations.rows
+	for (var index_of_weatherstation = 0; index_of_weatherstation < get_weatherstations.rowCount; index_of_weatherstation += 1) {
+		const row = get_weatherstations.rows[index_of_weatherstation]
+
+		result.push(get_new(row.ID, row.name, row.latitude, row.longitude))
 	}
 
 	return result;
 }
 
 function get_empty () {
+	return get_new(
+		undefined,
+		undefined,
+		undefined,
+		undefined
+	)
+}
+
+function get_new (ID, name, latitude, longitude) {
+	var new_url = undefined
+
+	if (ID != undefined) {
+		new_url = "/station/" + ID
+	}
+
 	return {
-		ID: undefined,
-		name: undefined,
-		latitude: undefined,
-		longitude: undefined
+		ID: ID,
+		name: name,
+		latitude: latitude,
+		longitude: longitude,
+		url: new_url
 	}
 }
