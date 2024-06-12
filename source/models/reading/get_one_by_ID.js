058618bbe141e3cf15e07359341a7a22f6cb97e8
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-13 00:02:09 +0200

    Added a module to load a reading by its id

---
+++ source/models/reading/get_one_by_ID.js
@@ -0,0 +1,31 @@
+import * as database from '../../database/database.js'
+
+import get_new_reading from './get_one_new.js'
+
+export default async function (ID) {
+	var result = undefined
+
+	try {
+		const query = await database.query(
+			'Select * from weathertop.readings where "ID" = $1::integer',
+			[ ID ]
+		)
+
+		if (query.rowCount === 1) {
+			const row = query.rows[0]
+
+			result = get_new_reading(row.ID, row.weatherstation_ID, row.weathercode, row.temperature, '°C', row.windspeed_in_kmh, 'km/h', row.airpressure_in_hpa, 'hpa', row.created_on)
+
+			console.info("Loaded weatherstation ID for reading ID")
+		}
+		else {
+			console.error("Could not find weatherstation ID for reading ID")
+		}
+	}
+	catch (error) {
+		console.error("Failed to load weatherstation ID for reading ID")
+		console.debug(error)
+	}
+
+	return result
+}
