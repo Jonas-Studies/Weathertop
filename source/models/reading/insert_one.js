c78e684c978f26c68cb33a929ae2c42d0573eeb4
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-14 11:24:43 +0200

    Added the new winddirection field to the insert module

--- source/models/reading/insert_one.js
+++ source/models/reading/insert_one.js
@@ -3,8 +3,8 @@ import * as database from '../../database/database.js'
 export default async function (reading) {
 	try {
 		const query = await database.query(
-			'Insert into weathertop.readings ("weatherstation_ID", "weathercode", "temperature_in_degrees", "windspeed_in_kmh", "airpressure_in_hpa") values ($1::integer, $2::integer, $3::float4, $4::float4, $5::float4)',
-			[ reading.weatherstation_ID, reading.weathercode, reading.temperature.value, reading.windspeed.value, reading.airpressure.value ]
+			'Insert into weathertop.readings ("weatherstation_ID", "weathercode", "temperature_in_degrees", "windspeed_in_kmh", "winddirection_in_degrees", "airpressure_in_hpa") values ($1::integer, $2::integer, $3::float4, $4::float4, $5::float4, $6::float4)',
+			[ reading.weatherstation_ID, reading.weathercode.key, reading.temperature.value, reading.windspeed.value, reading.winddirection.value, reading.airpressure.value ]
 		)
 		
 		console.info("Inserted reading")
