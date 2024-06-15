f2a25a5f7b932a62c85aaecbf0140efdb663f19b
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-15 13:57:03 +0200

    Changed the reading insert one module to match the changed get new reading module

--- source/models/reading/insert_one.js
+++ source/models/reading/insert_one.js
@@ -4,7 +4,7 @@ export default async function (reading) {
 	try {
 		const query = await database.query(
 			'Insert into weathertop.readings ("weatherstation_ID", "weathercode", "temperature_in_degrees", "windspeed_in_kmh", "winddirection_in_degrees", "airpressure_in_hpa") values ($1::integer, $2::integer, $3::float4, $4::float4, $5::float4, $6::float4)',
-			[ reading.weatherstation_ID, reading.weathercode.key, reading.temperature.value, reading.windspeed.value, reading.winddirection.value, reading.airpressure.value ]
+			[ reading.weatherstation_ID, reading.weathercode, reading.temperature, reading.windspeed, reading.winddirection, reading.airpressure ]
 		)
 		
 		console.info("Inserted reading")
