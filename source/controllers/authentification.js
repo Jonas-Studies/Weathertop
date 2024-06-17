e55cb334e76c0d39989a1e7d112ce53a70d0cb61
Jonas-Laptop <studies@jonas.zengerle.email>
2024-06-17 16:11:27 +0200

    Added validation for parameters to the authentification controller

--- source/controllers/authentification.js
+++ source/controllers/authentification.js
@@ -1,4 +1,6 @@
 import get_user_by_credentials from '../models/user/get_one_by_credentials.js'
+import is_username_valid from '../models/user/is_username_valid.js'
+import is_password_valid from '../models/user/is_password_valid.js'
 
 export async function display_login_page (request, response, next) {
 	response.render('login')
@@ -14,7 +16,9 @@ export async function create_session (request, response, next) {
 
 	var result = false
 
-	if (username != undefined && password != undefined) {
+	console.info('Recieved request to create session')
+
+	if (is_username_valid(username) === true && is_password_valid === true) {
 		const user = await get_user_by_credentials(username, password)
 
 		if (user != undefined) {
@@ -22,6 +26,10 @@ export async function create_session (request, response, next) {
 
 			result = true
 		}
+	}
+	else {
+		console.error('Invalid parameters for request')
+		console.debug(request.body)
 	}
 
 	response.send({ login_successfull: result })
